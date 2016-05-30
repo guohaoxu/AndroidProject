e100556666292b4c61336d94ac1af2065e4cbae5
guohao <guohao0817@gmail.com>
2016-05-30 11:03:36 +0800

    update

--- index.android.js
+++ index.android.js
@@ -1,111 +1,57 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * <ListView dataSource={this.state.dataSource} renderRow={this.renderMovie} style={styles.listView} />
+ * guohaoxu 2016-05-30
  */
-
 'use strict';
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  Image,
-  StyleSheet,
-  Text,
-  View,
-	ListView
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View
 } from 'react-native';
-
-const API_KEY = '7waqfqbprs7pajbz28mqf6vz';
-const API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
-const PAGE_SIZE = 25;
-const PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
-const REQUEST_URL = API_URL + PARAMS;
+import MyTouchableOpacity from './src/MyTouchableOpacity';
+import Movie from './src/Movie';
 
 class AndroidProject extends Component {
-  constructor(props) {
+	constructor(props) {
 		super(props);
-		this.state = {
-			dataSource: new ListView.DataSource({
-				rowHasChanged: (row1, row2) => row1 !== row2
-			}),
-			loaded: false
-		};
-  }
-	componentDidMount() {
-		this.fetchData();
 	}
-	fetchData() {
-		fetch(REQUEST_URL)
-			.then((response) => response.json())
-			.then((responseData) => {
-				this.setState({
-					dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
-					loaded: true
-			})
-			.done();
-		})
-	}
-  render() {
-		if (!this.state.loaded) {
-			return this.renderLoadingView();
-		}
-		return (
-			<Image source={require('./src/images/miao.jpg')} style={styles.thumbnail}>
-				<Text>Hi, I'm a miao.</Text>
-			</Image>
-
-		)
-  }
-	renderLoadingView() {
+	render() {
 		return (
 			<View style={styles.container}>
-				<Text>正在加载电影数据3...</Text>
+				<Text style={styles.title}>
+					Welcome to React Native, show cases!
+				</Text>
+				<Text style={styles.desc}>
+					This is a TouchableOpacity case.
+				</Text>
+				<MyTouchableOpacity text="确定" />
+				<MyTouchableOpacity text="取消" />
+				<Movie />
 			</View>
 		)
-	}
-	renderMovie(movie) {
-			return (
-				<View style={styles.container}>
-					<Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail} />
-					<View style={styles.rightContainer}>
-						<Text style={styles.title}>{movie.title}</Text>
-						<Text style={styles.year}>{movie.year}</Text>
-					</View>
-				</View>
-			)
 	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-		padding: 10,
+	container: {
 		flex: 1,
-		flexDirection: 'row',
-		justifyContent: 'center',
+		justifyContent: 'flex-start',
 		alignItems: 'center',
-		backgroundColor: '#F5FCFF'
-  },
-  thumbnail: {
-		width: 53,
-		height: 81,
-		borderRadius: 10
-  },
-  rightContainer: {
-		flex: 1,
-  },
-  title: {
-		fontSize: 20,
-		marginBottom: 8,
+		backgroundColor: '#f5f5f5',
+		padding: 20
+	},
+	title: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		color: '#c33'
+	},
+	desc: {
+		fontSize: 14,
+		color: '#999',
 		textAlign: 'center',
-  },
-  year: {
-		textAlign: 'center',
-  },
-	listView: {
-		paddingTop: 20,
-		backgroundColor: '#f5fcff'
-	}
+		marginTop: 5
+	},
 });
 
-AppRegistry.registerComponent('AndroidProject', () => AndroidProject);
+AppRegistry.registerComponent('AndroidProject', () => AndroidProject)
---
+++ src/Movie.js
@@ -0,0 +1,103 @@
+/**
+ * guohaoxu 2016-05-30
+ */
+
+'use strict';
+import React, { Component } from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+	ListView
+} from 'react-native';
+
+const API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+const API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+const PAGE_SIZE = 25;
+const PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+const REQUEST_URL = API_URL + PARAMS;
+
+export default class Movie extends Component {
+  constructor(props) {
+		super(props);
+		this.state = {
+			dataSource: new ListView.DataSource({
+				rowHasChanged: (row1, row2) => row1 !== row2
+			}),
+			loaded: false
+		};
+  }
+	componentDidMount() {
+		this.fetchData();
+	}
+	fetchData() {
+		console.log('a');
+		fetch(REQUEST_URL)
+			.then((response) => response.json())
+			.then((responseData) => {
+				this.setState({
+					dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+					loaded: true
+			})
+			.done();
+		})
+	}
+  render() {
+		if (!this.state.loaded) {
+			return this.renderLoadingView();
+		}
+		return (
+			<ListView dataSource={this.state.dataSource} renderRow={this.renderMovie} style={styles.listView} />
+		)
+  }
+	renderLoadingView() {
+		return (
+			<View style={styles.container}>
+				<Text>正在加载电影数据...</Text>
+			</View>
+		)
+	}
+	renderMovie(movie) {
+			return (
+				<View style={styles.container}>
+					<Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail} />
+					<View style={styles.rightContainer}>
+						<Text style={styles.title}>{movie.title}</Text>
+						<Text style={styles.year}>{movie.year}</Text>
+					</View>
+				</View>
+			)
+	}
+}
+
+const styles = StyleSheet.create({
+  container: {
+		padding: 10,
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF'
+  },
+  thumbnail: {
+		width: 53,
+		height: 81,
+		borderRadius: 10
+  },
+  rightContainer: {
+		flex: 1,
+  },
+  title: {
+		fontSize: 20,
+		marginBottom: 8,
+		textAlign: 'center',
+  },
+  year: {
+		textAlign: 'center',
+  },
+	listView: {
+		paddingTop: 20,
+		backgroundColor: '#f5fcff'
+	}
+});
